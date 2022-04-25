02945ff9c290ef70932f6a779c1255d05b8776c3
Ayatooo <louisreynard919@gmail.com>
2022-04-25 12:24:54 +0200

    :zap css split

--- webpack.config.js
+++ webpack.config.js
@@ -20,7 +20,8 @@ Encore
      * Each entry will result in one JavaScript file (e.g. app.js)
      * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
      */
-    .addEntry('app', './assets/app.js')
+    .addEntry('navbar-footer', './assets/navbar-footer.js')
+
 
     // enables the Symfony UX Stimulus bridge (used in assets/bootstrap.js)
     .enableStimulusBridge('./assets/controllers.json')
